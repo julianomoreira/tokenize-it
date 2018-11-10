a36a71606f8a09dec92916f11736253f5359009a
Juliano.Dasilva <juliano.dasilva@target.com>
2018-11-10 14:16:46 -0600

    set up React and first commit

---
+++ resources/webview.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './app.js'
+import pluginCall from 'sketch-module-web-view/client'
+
+ReactDOM.render(
+  <App />,
+  document.getElementById("root")
+)
+
+
+//
+// // Disable the context menu to have a more native feel
+// document.addEventListener("contextmenu", function(e) {
+//   e.preventDefault();
+// });
+//
+// document.getElementById('button').addEventListener('click', function () {
+//   pluginCall('nativeLog', 'Called from the webview')
+// })
+//
+// // called from the plugin
+// window.setRandomNumber = function (randomNumber) {
+//   document.getElementById('answer').innerHTML = 'Random number from the plugin: ' + randomNumber
+// }
